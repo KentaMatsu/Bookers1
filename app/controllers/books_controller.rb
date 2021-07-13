f125b6f5edadd66862d85ebce15eb34a55c39d55
KentaMatsu <matsuken102@gmail.com>
2021-07-13 12:47:15 +0000

    0713-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,21 @@ class BooksController < ApplicationController
 
   def show
   end
+  
+  def new
+  end
 
   def edit
   end
   
+  def create
+  end
   
+  def update
+  end
+  
+  def destroy
+  end
+
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  get 'top' => 'homestop'
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
